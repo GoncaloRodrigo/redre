fb69e961ce8a40fc592ea69bfe8f5143cfb9d87e
Develow <goncalo.rodrigo@tecnico.ulisboa.pt>
2016-09-29 01:00:07 +0100

    a

---
+++ server.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python           # This is client.py file
+
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+#host = socket.gethostname() # Get local machine name
+port = 59000                # Reserve a port for your service.
+
+s.connect(("Bernardo", port))
+
+s.send("Puta és tu?")
+print (s.recv(1024))
+s.close                     # Close the socket when done
